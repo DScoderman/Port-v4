394fe4572afaf844f1ecd8eca228cca81accd595
DSCoderman <dsenour6@gmail.com>
2023-07-18 16:11:51 -0400

    fixed link

--- port/src/Pages/Projects/data.js
+++ port/src/Pages/Projects/data.js
@@ -18,7 +18,7 @@ class Project {
 
 export const projectData = [
    new Project(image1,"The 19th Hole", "Helping you find the nearest golf courses and places to drink based off your zip code entered", "https://dscoderman.github.io/the-19th-hole/", "https://github.com/DScoderman/the-19th-hole"),
-   new Project(image2,"LFG Forum", "The staples for an online forum in finding people to game with", "www.google.com", "https://github.com/DScoderman/lfg-gameboard-Fork"),
+   new Project(image2,"LFG Forum", "The staples for an online forum in finding people to game with", "https://obscure-dusk-92996.herokuapp.com/login", "https://github.com/DScoderman/lfg-gameboard-Fork"),
    new Project(image3,"Password Generator", "Generating a password based on prompted inputs from user", "https://dscoderman.github.io/Password-Generator-V2/", "https://github.com/DScoderman/Password-Generator-V2"),
    new Project(image4,"E-commerce Database", "A backend system for connecting various retail goods in a database", "https://github.com/DScoderman/E-Commerce-Database", "https://github.com/DScoderman/E-Commerce-Database"),
    new Project(image5,"Text Browser Editor", "A progressive web app for downloading Javascript as a text editor", "https://young-dusk-78061.herokuapp.com/", "www.github.com/DScoderman"),
